b089833517255f6fd78a7233d3fcdbc2b3acd5d6
Ivaylo Ivanov <ivailo.ivanov3@gmail.com>
2016-03-17 16:21:58 +0200

    update2

--- SchoolRegister.Desktop/StudentWindow.xaml.cs
+++ SchoolRegister.Desktop/StudentWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SchoolRegister.Desktop
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for StudentWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StudentWindow : Window
     {
-        public MainWindow()
+        public StudentWindow()
         {
             InitializeComponent();
-        }
-
-        private void SignInClick(object sender, RoutedEventArgs e)
-        {
-            Daskala dask = new Daskala();
-            dask.ShowDialog();
         }
     }
 }
